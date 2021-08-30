c36d438ed674259edc4c5ff818f9b8700d4ce2cd
Laerson Castro <laersonjr@gmail.com>
2021-08-29 23:50:20 -0300

    Criando template da página Cadastro de Titulo

---
+++ src/main/java/com/laerson/treino/algaworks/springmvc/RepoTreinoPagamentoApiApplication.java
@@ -0,0 +1,13 @@
+package com.laerson.treino.algaworks.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoTreinoPagamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoTreinoPagamentoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laerson/treino/algaworks/springmvc/RepoTreinoPagamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laerson.treino.algaworks.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoTreinoPagamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
