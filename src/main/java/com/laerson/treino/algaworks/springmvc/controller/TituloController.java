4ba85f2732a50b27ebb0ef258c8818fdd461fa43
Laerson Castro <laersonjr@gmail.com>
2021-08-30 22:37:56 -0300

    Criando TituloRepository

--- src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
+++ src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
@@ -1,15 +1,30 @@
 package com.laerson.treino.algaworks.springmvc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.laerson.treino.algaworks.springmvc.model.Titulo;
+import com.laerson.treino.algaworks.springmvc.repository.TituloRepository;
 
 
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
 	
+	@Autowired
+	private TituloRepository tituloRepository;
+	
 	@RequestMapping("/novo")
 	public String novo() {
+		return "CadastroTitulo";
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		tituloRepository.save(titulo);
+		System.out.println(">>>>>" + titulo.getDescricao());
 		return "CadastroTitulo";
 	}
 
---
+++ src/main/java/com/laerson/treino/algaworks/springmvc/repository/TituloRepository.java
@@ -0,0 +1,9 @@
+package com.laerson.treino.algaworks.springmvc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.laerson.treino.algaworks.springmvc.model.Titulo;
+
+public interface TituloRepository extends JpaRepository<Titulo, Long>{
+
+}
