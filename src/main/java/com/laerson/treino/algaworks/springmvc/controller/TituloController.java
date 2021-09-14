0a147fcf55e72a363760d3e56ed576c95b5d2ca5
Laerson Castro <laersonjr@gmail.com>
2021-09-13 22:48:40 -0300

    Mantendo os valores no formulário

--- src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
+++ src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
@@ -1,6 +1,5 @@
 package com.laerson.treino.algaworks.springmvc.controller;
 
-
 import java.util.Arrays;
 import java.util.List;
 
@@ -12,51 +11,61 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.laerson.treino.algaworks.springmvc.model.StatusTitulo;
 import com.laerson.treino.algaworks.springmvc.model.Titulo;
 import com.laerson.treino.algaworks.springmvc.repository.TituloRepository;
 
-
 @Controller
 @RequestMapping("/titulos")
 public class TituloController {
-	
+
 	@Autowired
 	private TituloRepository tituloRepository;
-	
+
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
 		ModelAndView mv = new ModelAndView("CadastroTitulo");
-//		mv.addObject("todosStatusTitulo", StatusTitulo.values());
 		mv.addObject(new Titulo());
 		return mv;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
-	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return mv;
+			return "CadastroTitulo";
 		}
-		
+
 		tituloRepository.save(titulo);
-		
-		mv.addObject("mensagem", "Título salvo com sucesso!");
-		return mv;
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso!");
+		return "redirect:/titulos/novo";
 	}
-	
+
+//	@RequestMapping(method = RequestMethod.POST)
+//	public ModelAndView salvar(@Validated Titulo titulo, Errors errors) {
+//		ModelAndView mv = new ModelAndView("CadastroTitulo");
+//		if (errors.hasErrors()) {
+//			return mv;
+//		}
+//		
+//		tituloRepository.save(titulo);
+//		mv.addObject("mensagem", "Título salvo com sucesso!");
+////	Opção para limpar os dados quando o título é salvo com sucesso -->	mv.addObject(new Titulo());
+//		return mv;
+//	}
+
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView pesquisar() {
 		List<Titulo> todosTitulos = tituloRepository.findAll();
-		
+
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
 		return mv;
 	}
-	
+
 	@ModelAttribute("todosStatusTitulo")
-	public List<StatusTitulo> listaDosStatus(){
+	public List<StatusTitulo> listaDosStatus() {
 		return Arrays.asList(StatusTitulo.values());
 	}
 
