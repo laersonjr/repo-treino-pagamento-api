37842df3c9c8a9e19a5c337e4a0e272ec9858ddf
Laerson Castro <laersonjr@gmail.com>
2021-09-21 21:10:30 -0300

    Implementando a edição

--- src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
+++ src/main/java/com/laerson/treino/algaworks/springmvc/controller/TituloController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,10 +24,12 @@ public class TituloController {
 
 	@Autowired
 	private TituloRepository tituloRepository;
+	
+	private static final String CADASTRO_VIEW = "CadastroTitulo";
 
 	@RequestMapping("/novo")
 	public ModelAndView novo() {
-		ModelAndView mv = new ModelAndView("CadastroTitulo");
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
 		mv.addObject(new Titulo());
 		return mv;
 	}
@@ -34,7 +37,7 @@ public class TituloController {
 	@RequestMapping(method = RequestMethod.POST)
 	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) {
 		if (errors.hasErrors()) {
-			return "CadastroTitulo";
+			return "CADASTRO_VIEW";
 		}
 
 		tituloRepository.save(titulo);
@@ -61,6 +64,16 @@ public class TituloController {
 
 		ModelAndView mv = new ModelAndView("PesquisaTitulos");
 		mv.addObject("titulos", todosTitulos);
+		return mv;
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Long codigoTitulo) {
+		@SuppressWarnings("deprecation")
+		Titulo titulo = tituloRepository.getOne(codigoTitulo);
+		
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW); 
+		mv.addObject(titulo);
 		return mv;
 	}
 
